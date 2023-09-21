cd8fdb3c8abbcf1229be752cd5b68eef60867335
Beterraba <ls6182315@gmail.com>
2023-09-21 20:32:19 -0300

    feat: add middlewaresInitialize method

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,6 +4,12 @@ export default class App {
   public app: Application
   constructor() {
     this.app = express()
+    this.middlewaresInitialize()
+  }
+
+  private middlewaresInitialize() {
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: true }))
   }
 
   listen(port: number | string) {
