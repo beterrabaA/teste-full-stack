e99b8b21b9d1b634a39333cf5e8249525e324337
Beterraba <ls6182315@gmail.com>
2023-09-21 16:46:08 -0300

    feat:conditional token header exists

--- api/middlewares/auth.ts
+++ api/middlewares/auth.ts
@@ -5,5 +5,8 @@ export default class TokenValidator {
     req: Request,
     res: Response,
     next: NextFunction,
-  ) => {}
+  ) => {
+    const token = req.headers.authorization
+    if (!token) return res.status(401).json({ message: 'Token not found' })
+  }
 }
