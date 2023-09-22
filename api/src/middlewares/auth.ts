d81b4265aa55868ad05eefbc73b6259a7773857c
Lucas Vieira <ls6182315@gmail.com>
2023-09-22 15:38:33 -0300

    feat: authentication and user validation middle

--- api/src/middlewares/auth.ts
+++ api/src/middlewares/auth.ts
@@ -1,12 +1,35 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Response } from 'express'
+import { tokenDecoder } from '../utils/token'
+import UserService from '../services/user.service'
+import { CustomRequest, IUser } from '../types'
 
 export default class TokenValidator {
+  private service: UserService
+
+  constructor() {
+    this.service = new UserService()
+  }
+
   public validateJWT = async (
-    req: Request,
+    req: CustomRequest,
     res: Response,
     next: NextFunction,
   ) => {
     const token = req.headers.authorization
     if (!token) return res.status(401).json({ message: 'Token not found' })
+
+    try {
+      const decoded = tokenDecoder(token) as IUser
+      const loggedUser = await this.service.getUserById(decoded.id)
+
+      if (!loggedUser) {
+        return res.status(401).json({ message: 'Token must be a valid token' })
+      }
+
+      req.userId = loggedUser.id
+      next()
+    } catch (error) {
+      return res.status(401).json({ message: 'Token must be a valid token' })
+    }
   }
 }
