6dbd293064e28ba25c61613d1aa307fd2cb4ad09
Lucas Vieira <ls6182315@gmail.com>
2023-09-22 15:26:32 -0300

    feat: add login route

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 import UserController from '../controllers/user.controller'
+import JWTMiddleware from '../middlewares/auth'
 
 export default class UserRouter {
   public router: Router
   private controller: UserController
+  private auth: JWTMiddleware
   constructor() {
     this.router = Router()
     this.controller = new UserController()
+    this.auth = new JWTMiddleware()
     this.initializeRoutes()
   }
 
@@ -16,6 +19,9 @@ export default class UserRouter {
       this.controller.register.bind(this.controller),
     )
 
-    this.router.post('/login', this.controller.login.bind(this.controller))
+    this.router.post(
+      '/login',
+      this.controller.login.bind(this.controller),
+    )
   }
 }
