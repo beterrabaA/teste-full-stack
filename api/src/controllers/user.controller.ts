538e587a74bb34b0f9b81236e63a8087ccdbdf21
Lucas Vieira <ls6182315@gmail.com>
2023-09-22 15:42:31 -0300

    feat:add token generator in register controller

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
 import UserService from '../services/user.service'
+import { tokenGenerator } from '../utils/token'
+import { CustomRequest } from '../types'
 
 export default class UserController {
   private service
@@ -7,11 +9,20 @@ export default class UserController {
     this.service = new UserService()
   }
 
-  public async register(req: Request, res: Response): Promise<void> {
+  public async register(req: Request, res: Response): Promise<Response> {
     const { username, email, password } = req.body
-    console.log(username, password)
-
-    const user = await this.service.register(username, email, password)
-    res.json(user)
+    try {
+      const user = await this.service.register(username, email, password)
+      const token = tokenGenerator({
+        id: user.id,
+        email: user.email,
+        password: user.password,
+      })
+      return res.status(400).json({ token })
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ message: 'email already in use or password invalid' })
+    }
   }
 }
