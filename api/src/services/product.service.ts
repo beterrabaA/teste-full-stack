c8715c5482cc8014fc7ceca624b8c9f31dc1b450
Lucas Vieira <ls6182315@gmail.com>
2023-09-22 16:01:48 -0300

    feat: create getAll product service

--- api/src/services/product.service.ts
+++ api/src/services/product.service.ts
@@ -1,9 +1,24 @@
 import prismadb from '../lib/prismadb'
 import { Product } from '@prisma/client'
+import { IProduct } from '../types'
 
 export default class ProductService {
   private productModel
   constructor() {
     this.productModel = prismadb.product
+  }
+
+  async getAll(userId: string): Promise<Product[]> {
+    const productList = await this.productModel.findMany({
+      where: {
+        createdById: userId,
+      },
+    })
+
+    if (!productList) {
+      throw new Error('No products registered by user')
+    }
+
+    return productList
   }
 }
