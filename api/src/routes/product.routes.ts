6e1369d3b15fede0c4eaae45a9b931c9f4c26d6e
Lucas Vieira <ls6182315@gmail.com>
2023-09-22 20:19:51 -0300

    feat: add jwt middlewares in product route

---
+++ api/src/routes/product.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import ProductController from '../controllers/product.controller'
+import JWTMiddleware from '../middlewares/auth'
+
+export default class ProductRouter {
+  public router: Router
+  private controller: ProductController
+  private auth: JWTMiddleware
+  constructor() {
+    this.router = Router()
+    this.controller = new ProductController()
+    this.auth = new JWTMiddleware()
+    this.initializeMiddlewares()
+  }
+
+  public initializeMiddlewares() {
+    this.router.use(this.auth.validateJWT)
+  }
+}
