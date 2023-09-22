1e3a3a3b591e8c7209fac66e2eaf0a13740347db
Lucas Vieira <ls6182315@gmail.com>
2023-09-22 15:40:06 -0300

    feat: add external User type

--- api/src/utils/token.ts
+++ api/src/utils/token.ts
@@ -1,14 +1,10 @@
 import 'dotenv/config'
 
 import { sign, verify } from 'jsonwebtoken'
-
-interface User {
-  email: string
-  password: string
-}
+import { IUser } from '../types'
 
 const secret = process.env.JTW_SECRET as string
 
-export const tokenGenerator = (payload: User) => sign(payload, secret)
+export const tokenGenerator = (payload: IUser) => sign(payload, secret)
 
 export const tokenDecoder = (token: string) => verify(token, secret)
