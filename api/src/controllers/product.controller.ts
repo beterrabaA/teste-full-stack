6d61874258df1b7a7f011e83a7d3331e14460bd4
Lucas Vieira <ls6182315@gmail.com>
2023-09-23 15:17:30 -0300

    feat: add update product controller

--- api/src/controllers/product.controller.ts
+++ api/src/controllers/product.controller.ts
@@ -34,6 +34,7 @@ export default class ProductController {
   public async create(req: CustomRequest, res: Response): Promise<Response> {
     const { userId } = req
     const { name, description, category, price } = req.body
+
     if (!userId) {
       return res.status(401).json({ message: 'user id empty' })
     }
@@ -45,8 +46,32 @@ export default class ProductController {
         category,
         price,
       }
-      const product = await this.service.create(bodyProduct)
-      return res.status(201).json(product)
+      const newProduct = await this.service.create(bodyProduct)
+      return res.status(201).json(newProduct)
+    } catch (error) {
+      return res.status(400).json({ error })
+    }
+  }
+
+  public async update(req: CustomRequest, res: Response): Promise<Response> {
+    const { userId } = req
+    const { name, description, category, price } = req.body
+
+    if (!userId) {
+      return res.status(401).json({ message: 'user id empty' })
+    }
+
+    try {
+      const bodyProduct = {
+        createdById: userId,
+        name,
+        description,
+        category,
+        price,
+      }
+
+      const updatedProduct = await this.service.update(userId, bodyProduct)
+      return res.status(201).json(updatedProduct)
     } catch (error) {
       return res.status(400).json({ error })
     }
