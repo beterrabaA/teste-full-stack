11cf159a58b6853e01e466b59f1d771b41d7869f
Beterraba <ls6182315@gmail.com>
2023-09-24 18:20:15 -0300

    remove builded version

--- desafio-backend/api/dist/middlewares/error.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorMiddleware = void 0;
-function errorMiddleware(err, req, res, next) {
-    const { status, message } = err;
-    res.status(status).json({ status, message });
-}
-exports.errorMiddleware = errorMiddleware;
