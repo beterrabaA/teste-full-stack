c820d1517296a65bbc081d6fe60c4cda10207a81
Lucas Vieira <ls6182315@gmail.com>
2023-09-24 02:14:59 -0300

    fix name

--- desafio-backend/api/src/utils/token.ts
+++ desafio-backend/api/src/utils/token.ts
@@ -3,8 +3,8 @@ import 'dotenv/config'
 import { sign, verify } from 'jsonwebtoken'
 import { IUser } from '../types'
 
-const secret = process.env.JTW_SECRET as string
+const secret = process.env.JWT_SECRET as string
 
-export const tokenGenerator = (payload: IUser) => sign(payload, secret)
+export const tokenGenerator = (payload: IUser): string => sign(payload, secret)
 
 export const tokenDecoder = (token: string) => verify(token, secret)
