26c4a476a77e9e9be9fb203578a0a981a36e525e
Lucas Vieira <ls6182315@gmail.com>
2023-09-25 09:31:55 -0300

    next middleware

---
+++ desafio-backend/client/middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface CustomRequest extends NextRequest {
+    userId?: string;
+}
+
+export function middleware(req: CustomRequest) {
+    if (!req.userId) {
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
+}
+
+export const config = {
+  matcher: ["/"],
+};
