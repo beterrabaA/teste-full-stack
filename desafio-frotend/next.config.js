27eb4b5c4be6951a15f2a1e012428de2c96382f8
Lucas Vieira <ls6182315@gmail.com>
2023-09-26 21:44:56 -0300

    next config

--- desafio-frotend/next.config.js
+++ desafio-frotend/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    API_BASE_URL: 'https://api.punkapi.com/v2',
+  },
+  images: {
+    domains: ['images.punkapi.com'],
+  },
+}
 
 module.exports = nextConfig
