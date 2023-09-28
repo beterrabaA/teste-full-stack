79a7a40d6aa361a060c23edaf47a407f59c5ab54
Lucas Vieira <ls6182315@gmail.com>
2023-09-28 00:06:51 -0300

    feat: aux components

---
+++ desafio-frotend/constants/index.ts
@@ -0,0 +1,39 @@
+export const filter = ['abv', 'ibu', 'ebc']
+
+export const filterOptions = ['beer_name', 'yeast', 'hops', 'malt', 'food']
+
+export const filterOptionsText = [
+  { title: 'Name', value: 'beer_name' },
+  { title: 'Yeast', value: 'yeast' },
+  { title: 'Hops', value: 'hops' },
+  { title: 'Malt', value: 'malt' },
+  { title: 'Food', value: 'food' },
+]
+
+export const dateOptions = ['brewed_before', 'brewed_after']
+
+export const fuels = [
+  {
+    title: 'Fuel',
+    value: '',
+  },
+  {
+    title: 'Gas',
+    value: 'Gas',
+  },
+  {
+    title: 'Electricity',
+    value: 'Electricity',
+  },
+]
+
+export const order = [
+  {
+    title: 'Decreasing',
+    value: 'desc',
+  },
+  {
+    title: 'Increasing',
+    value: 'asc',
+  },
+]
