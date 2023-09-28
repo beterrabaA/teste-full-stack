df131a6c3b9e783720b49a807ca878e3b54c7e5c
Lucas Vieira <ls6182315@gmail.com>
2023-09-28 01:50:24 -0300

    fix: bugs

--- desafio-frotend/components/ShowMore.tsx
+++ desafio-frotend/components/ShowMore.tsx
@@ -9,18 +9,18 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter()
 
   const handleNavigation = () => {
-    const newLimit = (pageNumber + 1) * 10
+    const newLimit = (pageNumber + 1) * 12
     const newPathname = updateSearchParams('limit', newLimit.toString())
 
     router.push(newPathname)
   }
 
   return (
-    <div className="flex-center mt-10 w-full gap-5">
+    <div className="flex-center my-10 w-full gap-5">
       {!isNext && (
         <CustomButton
           btnType="button"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          containerStyles="bg-blue-300 text-white rounded-full mt-10"
           handleClick={handleNavigation}
           title="Show More"
         />
